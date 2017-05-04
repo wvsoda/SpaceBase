f2e38b56e23c8514ebcfc75a6cfa853b24f394f5
TJ178 <tjacques888@gmail.com>
2017-05-03 19:14:59 -0700

    Added the menu images again and added a menu that will be a start button

--- src/Levels/startMenu.java
+++ src/Levels/startMenu.java
@@ -1,5 +1,7 @@
 package Levels;
 
+import java.util.ArrayList;
+
 import Entities.*;
 
 public class startMenu extends level {
@@ -7,6 +9,10 @@ public class startMenu extends level {
 	public startMenu() {
 		Ship e = new Ship(300,200,390,500,0); 
 		ents.add(e);
+		
+		ArrayList<Entity> g = new ArrayList<Entity>();
+		Menu f = new Menu(700,600,200,50,g);
+		ents.add(f);
 	}
 
 	@Override
